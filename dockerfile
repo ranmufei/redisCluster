fe71ba2e95d15390df43cffc340d5826cb8cbf8e
ranmufei <ranmufei@qq.com>
2018-06-07 16:24:00 +0800

    edit dockerfile 构建默认从容器

--- dockerfile
+++ dockerfile
@@ -1,13 +1,14 @@
 #FROM redis:3-alpine
+# 本镜像构建为 从容器
 FROM hub.03in.com:5002/ranmufei/redis:alpine
 MAINTAINER ranmufei <ranmufei@qq.com>
 
 #COPY /etc/localtime /etc/localtime
 COPY localtime /etc/localtime
-COPY redis.conf /data/redis.conf
-COPY sentinel.conf /data/sentinel.conf
-COPY start.sh /data/start.sh
-RUN chmod +x /data/start.sh
-RUN chown redis:redis /data/*
-ENTRYPOINT ["sh","/data/start.sh"]
-CMD ["master"]
+#COPY redis.conf /data/redis.conf
+#COPY sentinel.conf /data/sentinel.conf
+COPY start.sh /start.sh
+RUN chmod +x /start.sh
+#RUN chown redis:redis /data/*
+ENTRYPOINT ["sh","/start.sh"]
+#CMD ["master"]
--- start.sh
+++ start.sh
@@ -1,26 +1,31 @@
+#!/bin/sh
 # @Author: ranmufei
 # @Date:   2018-06-07 10:35:22
 # @Last Modified by:   name
-# @Last Modified time: 2018-06-07 10:35:44
+# @Last Modified time: 2018-06-07 16:21:34
 
-cd /data
-redis_role=$1
-echo $redis_role
-if [ $redis_role = "master" ] ; then
-    echo "master"
-    sed -i "s/\$redis_port/$redis_port/g" redis.conf
-    redis-server /data/redis.conf
-elif [ $redis_role = "slave" ] ; then
-    echo "slave"
-    sed -i "s/\$redis_port/$redis_port/g" redis.conf
-    sed -i "s/#slaveof/slaveof/g" redis.conf
-    sed -i "s/\$master_port/$master_port/g" redis.conf
-    redis-server /data/redis.conf
-elif [ $redis_role = "sentinel" ] ; then
-    echo "sentinel"
-    sed -i "s/\$sentinel_port/$sentinel_port/g" sentinel.conf
-    sed -i "s/\$master_port/$master_port/g" sentinel.conf
-    redis-sentinel /data/sentinel.conf
-else
-    echo "unknow role!"
-fi     #ifend
+# 启动容器第一步骤注册为重容器
+redis-cli SLAVEOF $mastername 6379
+
+
+# cd /data
+# redis_role=$1
+# echo $redis_role
+# if [ $redis_role = "master" ] ; then
+#     echo "master"
+#     sed -i "s/\$redis_port/$redis_port/g" redis.conf
+#     redis-server /data/redis.conf
+# elif [ $redis_role = "slave" ] ; then
+#     echo "slave"
+#     sed -i "s/\$redis_port/$redis_port/g" redis.conf
+#     sed -i "s/#slaveof/slaveof/g" redis.conf
+#     sed -i "s/\$master_port/$master_port/g" redis.conf
+#     redis-server /data/redis.conf
+# elif [ $redis_role = "sentinel" ] ; then
+#     echo "sentinel"
+#     sed -i "s/\$sentinel_port/$sentinel_port/g" sentinel.conf
+#     sed -i "s/\$master_port/$master_port/g" sentinel.conf
+#     redis-sentinel /data/sentinel.conf
+# else
+#     echo "unknow role!"
+# fi     #ifend
